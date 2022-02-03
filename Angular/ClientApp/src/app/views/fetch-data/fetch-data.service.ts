fc4ecaffb3c2149ba90fe275586966f261ccded7
JHN38 <josselinh@hotmail.com>
2022-02-03 21:28:00 +0100

    Added a message service.

--- Angular/ClientApp/src/app/views/fetch-data/fetch-data.service.ts
+++ Angular/ClientApp/src/app/views/fetch-data/fetch-data.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WeatherForecast } from './fetch-data';
+import { MessageService } from '../../messages/messages.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,14 @@ import { WeatherForecast } from './fetch-data';
 export class WeatherService {
   public forecasts: WeatherForecast[] = [];
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string, private messageService: MessageService) {
     this.baseUrl = baseUrl + 'weatherforecast';
 
     console.log('WeatherForecastUrl: ' + baseUrl);
   }
 
   getForecasts(): Observable<WeatherForecast[]> {
+    this.messageService.add('Fetched the Forecasts');
     return this.http.get<WeatherForecast[]>(this.baseUrl);
   }
 }
